1c1699951c5854be8473bc89915f1e4ec09f4a5e
hashio111 <mnb7112es@gmail.com>
2019-09-14 14:25:28 +0900

    add-automatic-update
    自動更新機能を追加。ただし、まだどのページでも更新が発火する状態

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,23 @@
-$(function () {
+$(function() {
 
   function buildMessage(message) {
     var image = message.image ? `<img class="lower-message__image" src="${message.image}">` : '';
-    var html = `<div class="chat-main__messages__upper-message">
-    <div class="chat-main__messages__upper-message__user-name">
-    ${message.user_name}
-    </div>
-    <div class="chat-main__messages__upper-message__date">
-    ${message.created_at}
-    </div>
-    </div>
-    <div class="chat-main__messages__lower-message">
-      <p class="chat-main__messages__lower-message__content">
-      ${message.content}
-      </p>
-      ${image}
-    </div>`;
+    var html = `<div class="chat-main__messages__box" data-message-id="${message.id}">
+                  <div class="chat-main__messages__upper-message">
+                    <div class="chat-main__messages__upper-message__user-name">
+                    ${message.user_name}
+                    </div>
+                  <div class="chat-main__messages__upper-message__date">
+                    ${message.created_at}
+                  </div>
+                  </div>
+                <div class="chat-main__messages__lower-message">
+                  <p class="chat-main__messages__lower-message__content">
+                  ${message.content}
+                  </p>
+                ${image}
+                  </div>
+                </div>`;
     return html;
   }
 
@@ -44,4 +46,59 @@ $(function () {
         $(".form__submit").prop("disabled", false);
       })
   })
+
+ 
+
+  var reloadMessages = function() {
+    var group_id = $(".chat-main__main-header__left-box__group-name").data("group-id");
+    var last_message_id = $(".chat-main__messages__box").last().data("message-id");
+    $.ajax({
+      url: `/groups/${group_id}/api/messages`,
+      type: "GET",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log("success");
+      messages.forEach(function(message) {
+        var insertHTML = buildMessage(message);
+        $(".chat-main__messages").append(insertHTML);
+        $(".chat-main__messages").animate({scrollTop: $(".chat-main__messages")[0].scrollHeight});
+      })
+    })
+    .fail(function() {
+      console.log("error");
+    })
+  }
+
+  if (location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 5000);
+  }
+
+
+//   var interval = setInterval(function() {
+//     if (location.href.match(/\/groups\/\d+\/messages/)){
+//       $(".chat-main__messages").animate({scrollTop: $(".chat-main__messages")[0].scrollHeight});
+//       var group_id = $(".chat-main__main-header__left-box__group-name").data("group-id");
+//       var last_message_id = $(".chat-main__messages__box").last().data('message-id');
+//       $.ajax({
+//         url: `/groups/${group_id}/api/messages`,
+//         type: "GET",
+//         data: {id: last_message_id},
+//         dataType: "json"
+//       })
+//       .done(function(messages) {
+//         console.log("success");
+//         messages.forEach(function(message) {
+//           var insertHTML = buildMessage(message)
+//           $(".chat-main__messages").append(insertHTML)
+//         })
+//       })
+//       .fail(function() {
+//         alert('自動更新に失敗しました');
+//       });
+//     } else {
+//         clearInterval(interval);
+//       }
+//   } , 5000 );
 });
